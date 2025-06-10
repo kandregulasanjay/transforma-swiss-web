fa405d7940e5073396f91dd1bdde907a05a3029f
Sanjay555# <sanjaykandregula5@gmail.com>
2025-06-10 16:58:17 +0530

    git changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -184,7 +184,7 @@ const Home = () => {
         }}
       >
         <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-        <div className="relative z-10 flex flex-col text-center">
+        <div className="relative z-10 flex flex-col items-center text-center">
           <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl font-bold mb-6 animate-fade-in">
             {t.hero.title}
           </h1>
